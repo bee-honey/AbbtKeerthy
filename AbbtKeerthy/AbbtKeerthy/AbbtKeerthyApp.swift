81a73a5840fb40790b025e63a51d6bf1364f8061
bee-honey <honeybeeprogrammer@gmail.com>
2024-10-22 23:32:43 -0700

    Sample Hello world

---
+++ AbbtKeerthy/AbbtKeerthy/AbbtKeerthyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbbtKeerthyApp.swift
+//  AbbtKeerthy
+//
+//  Created by Naveen Keerthy on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct AbbtKeerthyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AbbtKeerthy/AbbtKeerthy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AbbtKeerthy
+//
+//  Created by Naveen Keerthy on 10/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Abbot!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
